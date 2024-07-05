5f6f90aa7bdc7e325a3fad7b28a159c8d914b341
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2024-07-05 12:54:04 -0400

    init

---
+++ README.md
@@ -0,0 +1,59 @@
+# QM-BepInExCompatibility
+
+A BepInEx mod that fixes Quasimorph Steam Workshop mod load issues when BepInEx is installed and that mod has reference dlls in their folder.
+
+This is *only* required if BepInEx is installed and running.
+
+# Issue
+When BepInEx is installed, a component called Doorstop prevents Steam Workshop mods from loading any dlls that are in the mod's directory.
+This is because Doorstop excludes searching the mod's folder and instead only searches the game's and  BepInEx's directories.
+
+Normally .NET will automatically search for and load any required dlls in the Steam Workshop mod's folder.
+
+This will cause file load exceptions when running the game.
+
+# Fix
+This mod fixes the issue by loading every dll located in a Quasimorph Workshop mod folder.
+
+When .NET tries to load a reference, the mod will use the dll that is already in memory instead of searching for a file  to load.
+
+Note that if a dll is loaded more than once, the subsequent loads will use the version in memory instead of loading the file again.
+
+# Important! Harmony Based Mods
+When creating Harmony based mods with BepInEx installed, the mod may work with BepInEx installed, but not without it.  Workshop users will not have BepInEx installed.
+
+Make sure to include the following files to the Steam Workshop mod folder for a Harmony based mod.
+
+```
+0Harmony.dll
+Mono.Cecil.dll
+MonoMod.RuntimeDetour.dll
+MonoMod.Utils.dll
+```
+
+
+The reason is BepInEx will load the modding dlls from the BepInEx directories.  When BepInEx is not installed or enabled, the mod will not have the required dlls.
+
+
+
+Test the mod without BepInEx running.  BepInEx can be disabled by renaming the winhttp.dll in the game's directory.
+
+## Other Mods Load Fixing
+A corner case would be if another Steam Workshop that has the required modding dlls is loaded before the user's mod was loaded.  
+
+It would be best to test the mod with no other subscribed mods.
+
+
+# Alternatives To This Mod
+## Custom dll Load
+Implement this same functionality by having the mod dll call the Assembly.LoadFrom for each dll in their mod folder.  Any mod that does not do this and has references will fail to load.
+
+## Change Doorstop Config
+
+Add every Steam Workshop mod's path in the ```doorstop_config.ini``` search path.
+
+```dll_search_path_override =``
+
+Example:
+
+```dll_search_path_override = <Steam Dir>>\steamapps\workshop\content\2059170\3282459391;<Steam Dir>>\steamapps\workshop\content\2059170\1234567890```
---
+++ src/Plugin.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BepInEx;
+
+namespace QM_BepInExCompatibility
+{
+    [BepInPlugin("nbk_redspy.QM-BepInExCompatibility", "QM-BepInExCompatibility", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        public static BepInEx.Logging.ManualLogSource Log { get; set; }
+
+        public void Awake()
+        {
+            Log = Logger;
+            LoadAllWorkshopDlls();
+        }
+
+        public static void LoadAllWorkshopDlls()
+        {
+            //Find the workshop directory.
+            string workshopPath = GetSteamWorkshopForGame("2059170");
+
+            Log.LogInfo($"QM_BepInExCompatibility loading all dlls at {workshopPath}");
+
+            int assemblyCount = 0;
+
+            try
+            {
+                //Load every assembly in every sub folder.
+                foreach (string dllPath in Directory.EnumerateFiles(workshopPath, "*.dll", SearchOption.AllDirectories))
+                {
+                    try
+                    {
+                        
+                        Log.LogInfo($"\t Loading: {dllPath}");
+                        Assembly.LoadFrom(dllPath);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        
+                        Log.LogError($"Error loading assembly {dllPath}");
+                        Log.LogError(ex);
+                    }
+
+                    assemblyCount++;
+                }
+
+                Log.LogInfo($"QM_BepInExCompatibility loaded {assemblyCount} assemblies");
+            }
+            catch (Exception ex)
+            {
+                Log.LogInfo("Error loading DLL's");
+                Log.LogError(ex);
+            }
+        }
+
+        public static string GetSteamInstallDirectory()
+        {
+            object installDir = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam", "InstallPath", null);
+
+            if (installDir is null)
+            {
+                throw new ApplicationException("Unable to find the Steam Directory from the Windows registry");
+            }
+
+            return (string)installDir;
+        }
+
+        public static string GetSteamWorkshopForGame(string gameId)
+        {
+            string workshopPath = Path.Combine(GetSteamInstallDirectory(), @"steamapps\workshop\content", gameId);
+
+            if (!Directory.Exists(workshopPath))
+            {
+                throw new ApplicationException($"Unable to find game's workshop at {workshopPath}");
+            }
+
+            return workshopPath;
+        }
+    }
+}
